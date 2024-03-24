41f20bbf973bb25d5ca8a710f085680c5b6dc26a
xinxilope <mateuseidt10@gmail.com>
2024-03-24 19:57:29 -0300

    arquivos iniciais

---
+++ combinar_arquivos_csv.py
@@ -0,0 +1,38 @@
+import os
+import pandas as pd
+
+def merge_csv_files(folder_path):
+    # Lista todos os arquivos na pasta
+    file_list = os.listdir(folder_path)
+    
+    # Filtra apenas os arquivos CSV
+    csv_files = [file for file in file_list if file.endswith('.csv')]
+    
+    # Verifica se há pelo menos um arquivo CSV na pasta
+    if not csv_files:
+        print("Não há arquivos CSV na pasta.")
+        return None
+    
+    # Inicializa um DataFrame vazio para armazenar os dados combinados
+    combined_df = pd.DataFrame()
+    
+    # Itera sobre os arquivos CSV e os combina em um DataFrame único
+    for file in csv_files:
+        file_path = os.path.join(folder_path, file)
+        df = pd.read_csv(file_path)
+        combined_df = pd.concat([combined_df, df], ignore_index=True)
+    
+    return combined_df
+
+# Pasta contendo os arquivos CSV
+folder_path = 'C:/Users/adm/Desktop/codigo_tcc/exports/JULHO'
+
+# Chama a função para combinar os arquivos CSV
+merged_df = merge_csv_files(folder_path)
+
+# Se o DataFrame combinado não estiver vazio, faça o que desejar com ele
+if merged_df is not None:
+    print("DataFrame combinado:")
+    print(merged_df)
+    # Você pode salvar o DataFrame combinado em um novo arquivo CSV, se desejar
+    merged_df.to_csv('combined_data.csv', index=False)
---
+++ pre_processamento.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import re
+import string
+import unicodedata
+import nltk
+from nltk.corpus import stopwords
+from emot.emo_unicode import UNICODE_EMOJI
+
+# Download de recursos do NLTK
+nltk.download('stopwords')
+stopwords_pt = set(stopwords.words('portuguese'))
+
+# adicionando outras stopwords
+minhas_stopwords = {}
+stopwords_pt.update(minhas_stopwords)
+
+def preprocess_text(text):
+    # Remove menções (strings começando com @)
+    text = re.sub(r'@[A-Za-z0-9_]+', '', text)
+
+    # Remove links
+    text = re.sub(r'https?://\S+|www\.\S+', '', text)
+    
+    # Remove hashtags (strings começando com #)
+    text = re.sub(r'#[A-Za-z0-9_]+', '', text)
+    
+    # Remove emojis
+    text = ''.join([char for char in text if char not in UNICODE_EMOJI])
+    
+    # Converte para minúsculas
+    text = text.lower()
+    
+    # Remove acentos
+    text = unicodedata.normalize('NFKD', text).encode('ASCII', 'ignore').decode('utf-8')
+    
+    # Remove pontuações
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    
+    # Remove stopwords em português
+    text = ' '.join([word for word in text.split() if word.lower() not in stopwords_pt])
+
+    return text
+
+# Carrega o DataFrame combinado
+combined_df = pd.read_csv('combined_data.csv')
+
+# Executa o pre processamento
+combined_df['text'] = combined_df['text'].apply(preprocess_text)
+
+# Remove duplicados com base na coluna 'url'
+combined_df.drop_duplicates(subset=['url'], inplace=True)
+
+# Remove linhas com texto vazio
+combined_df = combined_df[combined_df['text'] != '']
+
+# Salva o DataFrame pré-processado em um novo arquivo CSV
+combined_df.to_csv('preprocessed_data.csv', index=False)
+
+# Exibe o DataFrame pré-processado
+print("DataFrame pré-processado:")
+print(combined_df)
