8b35d398f8b596309629dad7249a33c5926087ad
xinxilope <mateuseidt10@gmail.com>
2024-09-14 02:21:07 -0300

    csv to text

---
+++ csv_to_txt.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Ler o arquivo CSV
+df = pd.read_csv('exports/preprocessed_data/preprocessed_data_julho.csv')
+
+# Abrir o arquivo TXT para escrita
+with open('exports/converted_data/data_julho.txt', 'w') as f:
+    # Iterar sobre cada linha do DataFrame
+    for _, row in df.iterrows():
+        # Obter o texto da coluna 'text'
+        texto = row['text']
+        # Substituir qualquer caractere especial por espaço (se necessário)
+        texto_limpo = texto.replace(',', '').replace('.', '')
+        # Escrever as palavras separadas por espaço
+        f.write(texto_limpo + '\n')
--- pre_processamento.py
+++ pre_processamento.py
@@ -64,7 +64,7 @@ def preprocess_text(text):
     return text
 
 # Carrega o DataFrame combinado
-combined_df = pd.read_csv('combined_data/combined_data.csv')
+combined_df = pd.read_csv('exports/combined_data_julho.csv')
 
 # Executa o pre processamento
 combined_df['text'] = combined_df['text'].apply(preprocess_text)
@@ -77,7 +77,7 @@ combined_df.drop_duplicates(subset=['text'], inplace=True)
 combined_df = combined_df[combined_df['text'] != '']
 
 # Salva o DataFrame pré-processado em um novo arquivo CSV
-combined_df.to_csv('preprocessed_data/preprocessed_data.csv', index=False)
+combined_df.to_csv('exports/preprocessed_data/preprocessed_data_julho.csv', index=False)
 
 # Exibe o DataFrame pré-processado
 print("DataFrame pré-processado:")
